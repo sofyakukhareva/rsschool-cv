1c5c87678519379bc615ec07d2a4f93723deed2b
sofyakukhareva <s.kuxareva@gmail.com>
2021-09-20 20:27:26 +0300

    feat: add burger menu

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const icon = document.querySelector('.menu__icon');
+const menu = document.querySelector('nav');
+
+if(icon){
+    icon.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
+        icon.classList.toggle('_active');
+        menu.classList.toggle('_active');
+    })
+}
+
 document.querySelectorAll('a[href^="#"').forEach(link => {
 
     link.addEventListener('click', function(e) {
@@ -12,9 +23,16 @@ document.querySelectorAll('a[href^="#"').forEach(link => {
         const elementPosition = scrollTarget.getBoundingClientRect().top;
         const offsetPosition = elementPosition - topOffset;
 
+        if(icon.classList.contains('_active')){
+            document.body.classList.remove('_lock');
+            icon.classList.remove('_active');
+            menu.classList.remove('_active');
+        }
+
         window.scrollBy({
             top: offsetPosition,
             behavior: 'smooth'
         });
     });
 });
+
